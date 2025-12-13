81ae506b31d71a199f22693415ef0ea77b67ace1
Yusaku-Watanabe-student <pc20040214@gmail.com>
2025-12-13 21:20:21 +0900

    Add files via upload

---
+++ mappings.py
@@ -0,0 +1,96 @@
+pref_map = {
+    '北海道': 'hokkaido',
+    '青森': 'aomori',
+    '岩手': 'iwate',
+    '宮城': 'miyagi',
+    '秋田': 'akita',
+    '山形': 'yamagata',
+    '福島': 'fukushima',
+
+    '茨城': 'ibaraki',
+    '栃木': 'tochigi',
+    '群馬': 'gunma',
+    '埼玉': 'saitama',
+    '千葉': 'chiba',
+    '東京': 'tokyo',
+    '神奈川': 'kanagawa',
+
+    '新潟': 'niigata',
+    '富山': 'toyama',
+    '石川': 'ishikawa',
+    '福井': 'fukui',
+    '山梨': 'yamanashi',
+    '長野': 'nagano',
+    '岐阜': 'gifu',
+    '静岡': 'shizuoka',
+    '愛知': 'aichi',
+
+    '三重': 'mie',
+
+    '滋賀': 'shiga',
+    '京都': 'kyoto',
+    '大阪': 'osaka',
+    '兵庫': 'hyogo',
+    '奈良': 'nara',
+    '和歌山': 'wakayama',
+
+    '鳥取': 'tottori',
+    '島根': 'shimane',
+    '岡山': 'okayama',
+    '広島': 'hiroshima',
+    '山口': 'yamaguchi',
+
+    '徳島': 'tokushima',
+    '香川': 'kagawa',
+    '愛媛': 'ehime',
+    '高知': 'kochi',
+
+    '福岡': 'fukuoka',
+    '佐賀': 'saga',
+    '長崎': 'nagasaki',
+    '熊本': 'kumamoto',
+    '大分': 'oita',
+    '宮崎': 'miyazaki',
+    '鹿児島': 'kagoshima',
+    '沖縄': 'okinawa'
+}
+
+pref_to_region = {
+    'hokkaido': 'hokkaido',
+
+    'aomori': 'tohoku', 'iwate': 'tohoku', 'miyagi': 'tohoku',
+    'akita': 'tohoku', 'yamagata': 'tohoku', 'fukushima': 'tohoku',
+
+    'ibaraki': 'kanto', 'tochigi': 'kanto', 'gunma': 'kanto',
+    'saitama': 'kanto', 'chiba': 'kanto', 'tokyo': 'kanto',
+    'kanagawa': 'kanto',
+
+    'niigata': 'chubu', 'toyama': 'chubu', 'ishikawa': 'chubu',
+    'fukui': 'chubu', 'yamanashi': 'chubu', 'nagano': 'chubu',
+    'gifu': 'chubu', 'shizuoka': 'chubu', 'aichi': 'chubu',
+
+    'mie': 'kansai',
+    'shiga': 'kansai', 'kyoto': 'kansai', 'osaka': 'kansai',
+    'hyogo': 'kansai', 'nara': 'kansai', 'wakayama': 'kansai',
+
+    'tottori': 'chugoku', 'shimane': 'chugoku', 'okayama': 'chugoku',
+    'hiroshima': 'chugoku', 'yamaguchi': 'chugoku',
+
+    'tokushima': 'shikoku', 'kagawa': 'shikoku',
+    'ehime': 'shikoku', 'kochi': 'shikoku',
+
+    'fukuoka': 'kyushu', 'saga': 'kyushu', 'nagasaki': 'kyushu',
+    'kumamoto': 'kyushu', 'oita': 'kyushu', 'miyazaki': 'kyushu',
+    'kagoshima': 'kyushu', 'okinawa': 'kyushu'
+}
+
+region_to_ja = {
+    'hokkaido': '北海道',
+    'tohoku': '東北',
+    'kanto': '関東',
+    'chubu': '中部',
+    'kansai': '関西',
+    'chugoku': '中国',
+    'shikoku': '四国',
+    'kyushu': '九州'
+}
